ab478635a4c1f2c6854e2d65bc49b70370a9f6e9
macbookpro16 <110rexlu@gmail.com>
2022-09-12 10:19:50 +0800

    update main func

--- main.go
+++ main.go
@@ -12,16 +12,6 @@ import (
 // 	"ginvue/MockServer/generate"
 // )
 
-// func main() {
-// 	generate.TotalMakeYaml()
-// }
-
-// func main() {
-// 	r := gin.Default()
-// 	r.GET("/UniversalMock", universalMockService)
-// 	r.Run(":8010")
-// }
-
 // 定义接收数据的结构体
 type Login struct {
 	// binding:"required"修饰的字段，若接收为空值，则报错，是必须字段
